fc5f9104a2f72237cadc64a40676c86304f5ff3c
luischipa <l.chipa.e@gmail.com>
2012-07-13 22:51:45 -0500

    cambios realizados

---
+++ prueba4/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prueba4::Application.config.secret_token = '3130c82108ae96ce2682efdecc79e292641f77be594f2f1133779f4eb5774af6564206a52971faa9255171119961769a009d474ce327f59fbdd2108d31af486b'
